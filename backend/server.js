9dd9402beb4687bf494e106f47b15bdc40df4f5b
Pabi691 <pabitra691@gmail.com>
2026-05-05 13:45:32 +0530

    change api end point

--- backend/server.js
+++ backend/server.js
@@ -74,8 +74,6 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV !== 'production') {
-    app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
-}
+app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
 
 module.exports = app;
